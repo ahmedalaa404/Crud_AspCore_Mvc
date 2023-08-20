04995dc0477ee9b391b62341ab52b3fbc849c9d8
ahmed alaa yassin <93731317+ahmedalaa404@users.noreply.github.com>
2023-08-20 06:08:09 +0300

    change in index File Of Role To accept Search Value From Form

--- CrudOperations/Controllers/RoleController.cs
+++ CrudOperations/Controllers/RoleController.cs
@@ -31,156 +31,168 @@ namespace CrudOperations.Controllers
 			}
 			else
 			{
+				var RolesNames = await _roleManager.FindByNameAsync(SearchValue);
+				var RoleViewModelSearch = new RoleViewModel()
+				{
+					id = RolesNames.Id,
+					RoleName = RolesNames.Name
+				} ;
+				var x = new List<RoleViewModel>()
+				{
+					RoleViewModelSearch
+
+				};
+				return View(x);
 
 
-				var RolesNames = await _roleManager.FindByNameAsync(SearchValue) as IEnumerable<IdentityRole>;
-				return View(RolesNames);
-			}
-
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		[HttpGet]
-		public async Task<IActionResult> Details(string id, string NameView = "Details")
-		{
-			if (id is null)
-				return BadRequest();
-
-			var user = await _roleManager.FindByIdAsync(id);
-			if (user is null)
-				return NotFound();
-
-			var RoleVm = new RoleViewModel()
-			{
-				RoleName = user.Name,
-				id = user.Id
 			};
-			return View(NameView, RoleVm);
-		}
-
-
-		#region update
-		[HttpGet]
-
-		public async Task<IActionResult> Update(string id)
-		{
-			return await Details(id, "Update");
 
 		}
-		[HttpPost]
+
+	
 
 
 
-		public async Task<IActionResult> Update([FromRoute] string id, RoleViewModel model)
+
+
+
+
+
+
+
+
+
+
+
+
+	[HttpGet]
+	public async Task<IActionResult> Details(string id, string NameView = "Details")
+	{
+		if (id is null)
+			return BadRequest();
+
+		var user = await _roleManager.FindByIdAsync(id);
+		if (user is null)
+			return NotFound();
+
+		var RoleVm = new RoleViewModel()
 		{
-			if (id != model.id)
-				return BadRequest(string.Empty);
+			RoleName = user.Name,
+			id = user.Id
+		};
+		return View(NameView, RoleVm);
+	}
 
 
-			if (ModelState.IsValid)
+	#region update
+	[HttpGet]
+
+	public async Task<IActionResult> Update(string id)
+	{
+		return await Details(id, "Update");
+
+	}
+	[HttpPost]
+
+
+
+	public async Task<IActionResult> Update([FromRoute] string id, RoleViewModel model)
+	{
+		if (id != model.id)
+			return BadRequest(string.Empty);
+
+
+		if (ModelState.IsValid)
+		{
+			try
 			{
-				try
-				{
-					var x = await _roleManager.FindByIdAsync(model.id);
-					x.Name = model.RoleName;
+				var x = await _roleManager.FindByIdAsync(model.id);
+				x.Name = model.RoleName;
 
-					var Result = await _roleManager.UpdateAsync(x);
+				var Result = await _roleManager.UpdateAsync(x);
 
-					if (Result.Succeeded)
-						return RedirectToAction(nameof(Index));
-				}
-				catch (System.Exception x)
-				{
-
-					ModelState.AddModelError(string.Empty, x.Message);
-					return View(model);
-				}
-
+				if (Result.Succeeded)
+					return RedirectToAction(nameof(Index));
 			}
-			else
+			catch (System.Exception x)
 			{
+
+				ModelState.AddModelError(string.Empty, x.Message);
 				return View(model);
 			}
+
+		}
+		else
+		{
 			return View(model);
-
-
 		}
-
-		#endregion
-
-
-		#region Delete
-		[HttpGet]
-		public async Task<IActionResult> Delete(string? id)
-		{
-			return await Details(id, "Delete");
-		}
-
-		[HttpPost]
-		public async Task<IActionResult> Delete([FromRoute] string id, RoleViewModel Model)
-		{
-			if (id != Model.id)
-				return BadRequest(string.Empty);
-			if (ModelState.IsValid)
-			{
-
-				var Role = await _roleManager.FindByIdAsync(Model.id);
-
-				await _roleManager.DeleteAsync(Role);
-
-				return RedirectToAction(nameof(Index));
-			}
-			else
-			{
-				return View(Model);
-			}
-		}
-		#endregion
-
-
-
-		#region Create
-		[HttpGet]
-		public IActionResult Create()
-		{
-
-			return View();
-		}
-
-
-		[HttpPost]
-		public async Task<IActionResult> Create(RoleViewModel Model)
-		{
-
-			var Role = new IdentityRole()
-			{
-				Name = Model.RoleName,
-				Id = Model.id
-			};
-
-			await _roleManager.CreateAsync(Role);
-
-
-			return RedirectToAction("index");
-		}
-		#endregion
-
-
-
+		return View(model);
 
 
 	}
+
+	#endregion
+
+
+	#region Delete
+	[HttpGet]
+	public async Task<IActionResult> Delete(string? id)
+	{
+		return await Details(id, "Delete");
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Delete([FromRoute] string id, RoleViewModel Model)
+	{
+		if (id != Model.id)
+			return BadRequest(string.Empty);
+		if (ModelState.IsValid)
+		{
+
+			var Role = await _roleManager.FindByIdAsync(Model.id);
+
+			await _roleManager.DeleteAsync(Role);
+
+			return RedirectToAction(nameof(Index));
+		}
+		else
+		{
+			return View(Model);
+		}
+	}
+	#endregion
+
+
+
+	#region Create
+	[HttpGet]
+	public IActionResult Create()
+	{
+
+		return View();
+	}
+
+
+	[HttpPost]
+	public async Task<IActionResult> Create(RoleViewModel Model)
+	{
+
+		var Role = new IdentityRole()
+		{
+			Name = Model.RoleName,
+			Id = Model.id
+		};
+
+		await _roleManager.CreateAsync(Role);
+
+
+		return RedirectToAction("index");
+	}
+	#endregion
+
+
+
+
+
+}
 }
